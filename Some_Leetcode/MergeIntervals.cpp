fa9106dfb05e969faf09e0ca281552a0a3b0a11a
Vidisha231106 <vidishadewan2020@gmail.com>
2026-02-28 14:10:43 +0530

    add implementation for merging intervals

---
+++ Some_Leetcode/MergeIntervals.cpp
@@ -0,0 +1,19 @@
+//56
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector <vector<int>> answer;
+        int n = intervals.size();
+        sort (intervals.begin(), intervals.end());
+        for (int i=0; i<n-1; i++){
+            if (intervals[i][1]<intervals[i+1][0]) answer.push_back(intervals[i]);
+            else {
+                intervals[i+1][0]=intervals[i][0];
+                intervals[i+1][1]=max(intervals[i][1], intervals[i+1][1]);
+            }
+        }
+        answer.push_back(intervals[n-1]);
+        return answer;
+    }
+};
