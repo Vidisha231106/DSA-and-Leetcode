4e924325f4e38db8035baa35b5eaab65ef777a2f
Vidisha231106 <vidishadewan2020@gmail.com>
2026-04-07 21:09:25 +0530

    add implementation for Coin Change problem

---
+++ DP/CoinChange.cpp
@@ -0,0 +1,18 @@
+// 322
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        sort(coins.begin(), coins.end());
+        vector<int> dp(amount+1, INT_MAX);
+        dp[0]=0;
+        for (int i=1; i<=amount; i++){
+            for (int j=0; j<n; j++){
+                if (i-coins[j]>=0 && dp[i-coins[j]]!=INT_MAX) dp[i]=min(dp[i-coins[j]]+1, dp[i]);
+            }
+        }
+        if (dp[amount]==INT_MAX) return -1;
+        return dp[amount];
+    }
+};
