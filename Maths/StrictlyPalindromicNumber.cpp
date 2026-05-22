01a067f29c59df1a2874be1c40a887b37f15819f
Vidisha231106 <vidishadewan2020@gmail.com>
2026-05-22 23:11:38 +0530

    IMPORTANT Q - READ THE MATHS used here

---
+++ Maths/StrictlyPalindromicNumber.cpp
@@ -0,0 +1,28 @@
+//2396
+
+/*
+Represent n in base n−2
+
+Let
+
+b=n−2
+
+Now divide n by b:
+
+n=1⋅(n−2)+2
+
+So in base n−2, the digits are:
+
+12
+
+because:
+
+quotient = 1
+remainder = 2
+
+So:
+
+n=12
+(n−2)
+	​
+*/
