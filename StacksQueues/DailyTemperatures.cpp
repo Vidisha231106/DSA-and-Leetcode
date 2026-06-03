7491d7c92e29d6b48121f4315844596406333084
Vidisha231106 <vidishadewan2020@gmail.com>
2026-06-03 11:17:01 +0530

    better approach

--- StacksQueues/DailyTemperatures.cpp
+++ StacksQueues/DailyTemperatures.cpp
@@ -18,3 +18,27 @@ public:
         return answer;
     }
 };
+
+/*Better Code:
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+       int n = temperatures.size();
+        vector<int> ans(n, 0);
+        int hottest = 0;
+        
+        for (int i = n - 1; i >= 0; --i) {
+            if (temperatures[i] >= hottest) {
+                hottest = temperatures[i];
+                continue;
+            }
+            int days = 1;
+            while (temperatures[i + days] <= temperatures[i])
+                days += ans[i + days];
+            ans[i] = days;
+        }
+        return ans;
+    }
+    
+};*/
