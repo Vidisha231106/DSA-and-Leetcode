d591c8a97160f3f044644057f601fae669b58b65
Vidisha231106 <vidishadewan2020@gmail.com>
2026-06-26 09:56:39 +0530

    refactor checkIfPrerequisite method to use adjacency matrix and remove unnecessary DFS

--- Graphs/DFS/CourseSchedule4.cpp
+++ Graphs/DFS/CourseSchedule4.cpp
@@ -2,44 +2,24 @@
 
 class Solution {
 public:
-    void dfs(vector<vector<int>> &v, int index, stack<int>& st, vector<int>& visited){
-        if (index==visited.size()) return;
-        if (visited[index]==1) return;
-        visited[index]=1;
-        int n=v[index].size();
-        for (int i=0; i<n; i++){
-            dfs(v, v[index][i], st, visited);
-        }
-        st.push(index);
-        visited[index]=0;
-    }
     vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
-        vector<vector<int>> v(numCourses);
-        int n= prerequisites.size();
+        vector<vector<int>>v (numCourses, vector<int>(numCourses, 0));
+        int n=prerequisites.size();
         for (int i=0; i<n; i++){
-            v[prerequisites[i][1]].push_back(prerequisites[i][0]);
+            v[prerequisites[i][0]][prerequisites[i][1]]=1;
         }
-        stack <int> st;
-        vector<int> visited(numCourses);
-        for (int i=0; i<numCourses; i++){
-            if (visited[i]==0) dfs(v, i, st, visited);
-        }
-        vector <int> answer;
-        while (st.empty()!=true){
-            answer.push_back(st.top());
-            st.pop();
-        }
-        unordered_map <int, int> mp; //course: index
-        n=answer.size();
-        vector<bool> ans;
-        for (int i=0; i<n; i++){
-            mp[answer[i]]=i;
+        for (int k=0; k<numCourses; k++){
+            for (int i=0; i<numCourses; i++){
+                for (int j=0; j<numCourses; j++){ 
+                    v[i][j]=(v[i][j] || (v[i][k] && v[k][j]));
+                }
+            }
         }
         n=queries.size();
+        vector<bool> answer(n, false);
         for (int i=0; i<n; i++){
-            if (mp[queries[i][0]]<mp[queries[i][1]]) ans.push_back(false);
-            else ans.push_back(true);
+            if (v[queries[i][0]][queries[i][1]]==1) answer[i]=true;
         }
-        return ans;
+        return answer;
     }
 };
