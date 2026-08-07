bc24f0524ad4dfafef8e374fa1bc3ecf319a3a7d
Vidisha <vidishadewan2020@gmail.com>
2026-08-07 10:37:35 +0530

    add implementation for Recent Counter problem

---
+++ StacksQueues/NumberOfRecentCalls.cpp
@@ -0,0 +1,21 @@
+// 933
+
+class RecentCounter {
+public:
+    queue <int> q;
+    int count;
+    RecentCounter() {
+        count=0;
+    }
+    int ping(int t) {
+        q.push(t);
+        while (abs(q.front()-t)>3000) q.pop();
+        return q.size();
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
