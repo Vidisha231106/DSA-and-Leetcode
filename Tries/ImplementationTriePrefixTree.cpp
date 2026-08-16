50ef3bdc9ecc912932bae8fd48c7bdf42f69a9bc
Vidisha <vidishadewan2020@gmail.com>
2026-08-16 17:09:23 +0530

    add implementation for Trie data structure

---
+++ Tries/ImplementationTriePrefixTree.cpp
@@ -0,0 +1,73 @@
+// 208
+
+struct Node{
+    vector<Node*> links=vector<Node*>(26, nullptr);
+    bool flag=false;
+    bool containsKey(char ch){
+        return (links[ch-'a']!=nullptr);
+    }
+    void putCh(char ch, Node* node){
+        links[ch-'a']=node;
+    }
+    Node* get(char ch){
+        return links[ch-'a'];
+    }
+    void setEnd(){
+        flag=true;
+    }
+    bool isEnd(){
+        return flag;
+    }
+    ~Node(){
+        for (int i=0; i<26; i++){
+            if (links[i]!=nullptr) {
+                delete links[i];
+                links[i]=nullptr;
+            }
+        }
+    }
+};
+class Trie {
+private: Node* root;
+public:
+    Trie() {
+        root= new Node();
+    }
+    
+    void insert(string word) {
+        Node* node=root;
+        for (char ch: word){
+            if (node->containsKey(ch)==false){
+                node->putCh(ch, new Node());
+            }
+            node=node->get(ch);
+        }
+        node->setEnd();
+    }
+    
+    bool search(string word) {
+        Node* node=root;
+        for (char ch:word){
+            if (node->containsKey(ch)!=true) return false;
+            node=node->get(ch);
+        }
+        return node->isEnd();
+    }
+    
+    bool startsWith(string prefix) {
+        Node* node=root;
+        for (char ch: prefix){
+            if (node->containsKey(ch)!=true) return false;
+            node=node->get(ch);
+        }
+        return true;        
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
