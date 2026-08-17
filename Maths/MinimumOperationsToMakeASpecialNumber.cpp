2a1d338b25dcb634f6ed9876fd9d03d97de7bed3
Vidisha <vidishadewan2020@gmail.com>
2026-08-17 19:26:21 +0530

    add implementation for Minimum Operations to Make a Special Number problem with O(n) solution

--- Maths/MinimumOperationsToMakeASpecialNumber.cpp
+++ Maths/MinimumOperationsToMakeASpecialNumber.cpp
@@ -1,5 +1,7 @@
 // 2844
 
+
+//O(n^2) solution
 class Solution {
 public:
     int minimumOperations(string num) {
@@ -34,6 +36,52 @@ public:
                 ans=min(ans, (n-1-mp[0][j])+mp[0][j]-mp[0][i]-1);
             }
         }
+        return ans;
+    }
+};
+
+//O(n) solution
+
+class Solution {
+public:
+    int minimumOperations(string num) {
+        unordered_map <int,vector<int>> mp;
+        int n=num.length();
+        for (int i=0; i<n; i++){
+            if (num[i]=='0' ||num[i]=='7'||num[i]=='2'||num[i]=='5') mp[num[i]-'0'].push_back(i);
+        }
+        int ans=(mp[0].size()==0) ? n: n-1;
+        int flag=-1;
+        for (int i=n-1; i>=0; i--){
+            if (flag==-1 && num[i]=='5') flag=i;
+            if (flag!=-1 && num[i]=='2'){
+                ans=min(ans, (n-1-flag)+flag-i-1);
+            }
+        }
+        flag=-1;
+        for (int i=n-1; i>=0; i--){
+            if (flag==-1 && num[i]=='0') flag=i;
+            if (flag!=-1 && num[i]=='5'){
+                ans=min(ans, (n-1-flag)+flag-i-1);
+            }
+        }
+        flag=-1;
+        for (int i=n-1; i>=0; i--){
+            if (flag==-1 && num[i]=='5') flag=i;
+            if (flag!=-1 && num[i]=='7'){
+                ans=min(ans, (n-1-flag)+flag-i-1);
+            }
+        }
+        flag=-1;
+        for (int i=n-1; i>=0; i--){
+            if (flag==-1 && num[i]=='0') {
+                flag=i;
+                continue;
+            }
+            if (flag!=-1 && num[i]=='0'){
+                ans=min(ans, (n-1-flag)+flag-i-1);
+            }
+        }        
         return ans;
     }
 };
